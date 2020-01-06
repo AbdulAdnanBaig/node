28001f5257858b16d02e700389f938145f69abd1
AbdulAdnanBaig <adnanabdul8@gmail.com>
2020-01-06 15:11:37 +0530

    Master: cors

--- app.js
+++ app.js
@@ -4,9 +4,22 @@ const bodyParser = require('body-parser');
 const product = require('./routes/product.route'); // Imports routes for the products
 var app = express();
 const product_details = require('./controllers/product.controller');
-app.configure(function () {
-    app.use(allowCrossDomain);
-  });
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain);
 app.use('/products', product);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));

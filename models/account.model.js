b5a258d5438e73540bb7ac90621915c86fad1aca
AbdulAdnanBaig <adnanabdul8@gmail.com>
2020-01-18 19:08:08 +0530

    Added new column to the Account model

--- models/account.model.js
+++ models/account.model.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 let AccountSchema = new Schema({
     uId: {type: Number, required: false},
-    name: {type: String, required: false}
+    name: {type: String, required: false},
+    manager: {type: String, required: false},
 });
 
 

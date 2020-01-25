0c3e02fd366b702699c9cee5ec5e9faa761d2ba3
AbdulAdnanBaig <adnanabdul8@gmail.com>
2020-01-25 19:54:05 +0530

    Added object response to the job API

--- controllers/job.controller.js
+++ controllers/job.controller.js
@@ -1,10 +1,21 @@
 const Job = require('../models/job-opening.model');
+const Acc = require('../models/account.model');
+var test
 
 // Job section API's
 exports.job_get = function (req, res) {
     Job.find(function (err, resp) {
-        if (err) return next(err);
-        res.send(resp);
+        // if (err) return next(err);      
+        for (let index = 0; index < resp.length; index++) {
+            const element = resp[index];
+            // find query to populate the account details
+            Acc.find({ 'uId' : element.account },
+            function(req, res){
+                element.account = res;
+                console.log(element);
+            });
+        }
+
     });
 };
 
--- models/job-opening.model.js
+++ models/job-opening.model.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 let JobSchema = new Schema({
     jId: { type: Number, required: false },
-    account: { type: Number, required: false },
+    account: { },
     designation: { type: Number, required: false },
     status: { type: String, required: false },
     jobDes: { type: String, required: false },
